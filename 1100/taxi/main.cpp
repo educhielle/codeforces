3c63c2931053758d02ab463462bb967f71cbc66a
Edu <25762736+educhielle@users.noreply.github.com>
2023-10-01 18:26:48 -0300

    taxi

--- 1100/taxi/main.cpp
+++ 1100/taxi/main.cpp
@@ -5,25 +5,25 @@ using namespace std;
 
 int main()
 {
-    int n, t = 0, x;
-    vector<int> v(5);
+    int n, s;
+    vector<int> g(5); // number of groups of size 'i' for 1 <= i <= 4
     cin >> n;
     while (n--)
     {
-        cin >> x;
-        v[x]++;
+        cin >> s; // read size of a group
+        g[s]++;   // increment of the counter with the number of groups of size 's'
     }
 
-    int r = v[4];
-    r += v[2] / 2;
-    v[2] &= 1;
-    if (v[3] >= v[1])
-        r += v[3] + v[2];
+    int t = g[4];  // each g[4] needs a taxi
+    t += g[2] / 2; // two g[2]s add to 4
+    g[2] &= 1;     // g[2] may be odd
+    if (g[3] >= g[1])
+        t += g[3] + g[2]; // g[3] cannot be combined with g[2]
     else
     {
-        v[1] += -v[3] + (v[2] << 1);
-        r += v[3] + (v[1] >> 2) + ((v[1] & 3) != 0);
+        g[1] += -g[3] + (g[2] << 1); // g[1]+g[3] = 1 taxi, g[2] = 2*g[1]
+        t += g[3] + (g[1] >> 2) + ((g[1] & 3) != 0); // add the taxi numbers
     }
     
-    cout << r << '\n';
+    cout << t << '\n';
 }
